db2e6ab69bbcd9a1ad93dda1a0bdd1e5fc13de22
Shelinh <dsk2.com@gmail.com>
2025-11-03 23:44:54 +0000

    metodo_enviar_mensagem

--- canais/whatsapp.cs
+++ canais/whatsapp.cs
@@ -5,10 +5,24 @@ public class whatsapp
             console.WriteLine($"Enviando mensagem de texto para numero {numero}");
 
         }
+
+public void EnviarMensagem(string numero, VideoMessage message)
+        {
+            console.WriteLine($"Enviando mensagem de video para numero {numero}");
+
+        }
+
+public void EnviarMensagem(string numero, PhotoMessage message)
+        {
+            console.WriteLine($"Enviando mensagem de foto para numero {numero}");
+
+        }
+
+
+public void EnviarMensagem(string numero, FileMessage message)
+        {
+            console.WriteLine($"Enviando mensagem de arquivo para numero {numero}");
+
+        }
+
 }
-
-
-
-arquivo 
-foto 
-video 
