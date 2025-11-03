e28530f7fa5b7def76725ae7a795f2a2c7ba9e12
Shelinh <dsk2.com@gmail.com>
2025-11-03 23:40:15 +0000

    whatsapp

---
+++ canais/whatsapp.cs
@@ -0,0 +1,14 @@
+public class whatsapp
+{
+        public void EnviarMensagem(string numero, TextMessage message)
+        {
+            console.WriteLine($"Enviando mensagem de texto para numero {numero}");
+
+        }
+}
+
+
+
+arquivo 
+foto 
+video 
