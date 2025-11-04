8b558a5e8af88cbf8796dd09c0dd144bdd635886
Shelinh <dsk2.com@gmail.com>
2025-11-04 00:25:53 +0000

    class_restantes

--- Metodos_mensagem/TextMessage.cs
+++ Metodos_mensagem/TextMessage.cs
@@ -1,9 +1,9 @@
 public class TextMessage : MessageBase
 {
 
-    public TextMessage(string message):base(message)
+    public TextMessage(string message): base(message)
     {
-        
+
     }
 
 }
